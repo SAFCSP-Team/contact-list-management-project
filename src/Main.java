dcd4b29779604016297876ed89f1ee17dcf4270c
lamabeta <148013077+lamabeta@users.noreply.github.com>
2025-08-17 15:26:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-      /* add your code here */
+        ContactList myContacts = new ContactList();
+        myContacts.add(new Contact("Lana", "0600000000"));
+        myContacts.add(new Contact("Sara", "0607777777"));
+        myContacts.add(new Contact("Samaa", "0611111111"));
+
+        myContacts.allContacts();
+
+        myContacts.call("Lana");
+        myContacts.call("Sara"); 
+
+        myContacts.closeCall();
+
+        myContacts.call("Sara");
+
+        myContacts.update("Samaa","0622222222"));
+        myContacts.delete("Lana");
+
+        myContacts.allContacts();
     }
 
 }
